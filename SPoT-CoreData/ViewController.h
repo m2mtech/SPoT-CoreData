790bfb3ada020e687c634050f94f69df0dbe4cae
m2m tech <tech@m2m.at>
2013-03-11 13:47:57 +0100

    Initial Commit

---
+++ SPoT-CoreData/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  SPoT-CoreData
+//
+//  Created by Martin Mandl on 11.03.13.
+//  Copyright (c) 2013 m2m server software gmbh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
